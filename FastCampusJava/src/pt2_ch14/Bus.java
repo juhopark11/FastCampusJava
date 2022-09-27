408c9c4e9133bd628986ad2de34e02c714f436bc
Juho Park <todarii@naver.com>
2022-09-27 12:40:53 +0900

    .

--- FastCampusJava/src/pt2_ch14/Bus.java
+++ FastCampusJava/src/pt2_ch14/Bus.java
@@ -11,12 +11,12 @@ public class Bus {
 	}
 	
 	// 이 부분이 협력이 일어나고 있다. 
-	public void take(int money) {
+	public void take(int money) { 
 		this.money += money;
 		passengerCount ++; //돈을 받았다는 것은 승객이 늘었다는 말. 
 	}
 	
 	public void showBusInfo() {
-		System.out.println("승객수는"+passengerCount+"이고, 보유수익은"+money+"입니다.");
+		System.out.println("승객수"+passengerCount+"이고, 보유수익은"+money+"입니다.");
 	}
 }
